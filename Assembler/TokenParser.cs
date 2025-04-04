6bc84f1867041176aa0fff9f9468e95dd20968a8
kinderhead <67707412+kinderhead@users.noreply.github.com>
2025-04-04 14:23:04 -0400

    work

---
+++ Assembler/TokenParser.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Text;
+
+namespace Assembler
+{
+    public abstract class TokenParser
+    {
+        public abstract bool IsValid(StringBuilder soFar, char c);
+    }
+
+    public class ConstantToken(string text) : TokenParser
+    {
+        public readonly string Text = text;
+
+        public override bool IsValid(StringBuilder soFar, char c) => soFar.Length < Text.Length && Text[soFar.Length - 1] == c;
+    }
+
+    public class CollectiveOptionToken(char[] options) : TokenParser
+    {
+        public readonly char[] Options = options;
+
+        public override bool IsValid(StringBuilder soFar, char c) => Options.Contains(c);
+    }
+
+    public class TextToken : TokenParser
+    {
+        public override bool IsValid(StringBuilder soFar, char c)
+        {
+            if (c == '$' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) return true;
+            else if (soFar.Length != 0 && (c == '#' || (c >= '0' && c <= '9'))) return true;
+            else return false;
+        }
+    }
+
+    public class LabelToken : TokenParser
+    {
+        public override bool IsValid(StringBuilder soFar, char c)
+        {
+            if (soFar[-1] == ':') return false;
+            else if (c == '$' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) return true;
+            else if (soFar.Length != 0 && (c == '#' || c == ':' || (c >= '0' && c <= '9'))) return true;
+            else return false;
+        }
+    }
+
+    public class IntegerToken : TokenParser
+    {
+        public override bool IsValid(StringBuilder soFar, char c)
+        {
+            c = char.ToLower(c);
+
+            if (soFar.Length == 0 && c >= '0' && c <= '9') return true;
+            else if (soFar.Length == 1 && (c == 'x' || c == 'b' || (c >= '0' && c <= '9'))) return true;
+            else if (soFar.Length >= 2)
+            {
+                if (soFar[1] == 'x' && ((c >= 'a' && c <= 'f') || (c >= '0' && c <= '9'))) return true;
+                else if (soFar[1] == 'b' && (c == '0' || c == '1')) return true;
+                else if (c >= '0' && c <= '9') return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class RegisterToken : TokenParser
+    {
+        public override bool IsValid(StringBuilder soFar, char c)
+        {
+            if (soFar.Length == 0 && c == 'r') return true;
+            else if (soFar.Length == 1 && c >= '0' && c <= '9') return true;
+            else if (soFar.Length == 2 && soFar[1] == '1' && c >= '0' && c <= '5') return true;
+            else return false;
+        }
+    }
+
+    public class StringToken : TokenParser
+    {
+        public override bool IsValid(StringBuilder soFar, char c)
+        {
+            if (soFar.Length == 0 && c == '"') return true;
+            else if (soFar.Length >= 2 && soFar[-1] != '"') return true;
+            else if (soFar.Length >= 2 && soFar[-1] == '"' && soFar[-2] == '\\') return true;
+            return false;
+        }
+    }
+}
